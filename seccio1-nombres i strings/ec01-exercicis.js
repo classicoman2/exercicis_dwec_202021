62cfab6a186216a937a516677466920f831bafed
toni <aamengual@cifpfbmoll.eu>
2020-10-06 19:45:22 +0200

    first commit

--- seccio1-nombres i strings/ec01-exercicis.js
+++ seccio1-nombres i strings/ec01-exercicis.js
@@ -1,9 +1,10 @@
 
 function numero_signe() {
   //capturar contingut de camp "input1"
-  input1 = document.getElementById("input1").value;
 
-  console.log("El numero introduit es " + input1 )
+  input1 = document.getElementById("input1").value;
+  
+console.log("El numero introduit es " + input1 )
 
   if (input1 > 0) {
     document.getElementById("results").innerHTML = "POSITIU"
